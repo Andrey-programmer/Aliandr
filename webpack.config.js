06471f08aeae2081644f7d3ca841b388ea14511a
Andrey <lak0sta1302@gmail.com>
2020-07-04 17:50:32 +0300

    Изменил прайс, обновил библиотеки

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ const babelLoader = () => {
     }]
 
     if (isDev) {
-        loader.push({loader: 'eslint-loader'})
+        // loader.push({loader: 'eslint-loader'})
     }
 
     return loader
